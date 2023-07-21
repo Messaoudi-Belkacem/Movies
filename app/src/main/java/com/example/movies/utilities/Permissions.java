937824437ebb816e48c19bcfa653dcc25bc2c7ee
HP <wailmessaoudi806@gmail.com>
2023-07-21 12:52:49 +0100

    Creation of a new utility class
    Created Permission utility class for requesting read external storage permission

---
+++ app/src/main/java/com/example/movies/utilities/Permissions.java
@@ -0,0 +1,57 @@
+package com.example.movies.utilities;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.example.movies.Manifest;
+
+public class Permissions extends AppCompatActivity {
+
+    private Permissions() {}
+
+    public static final int STORAGE_PERMISSION_CODE = 1;
+
+    public static void checkIfPermissionIsGranted(Activity activity) {
+        if (ContextCompat.checkSelfPermission(activity, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(activity, "Permission is already granted", Toast.LENGTH_SHORT).show();
+        } else {
+            requestStoragePermission(activity);
+        }
+    }
+
+    private static void requestStoragePermission(Activity activity) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            new AlertDialog.Builder(activity)
+                    .setTitle("Permission needed")
+                    .setMessage("This permission is needed to access your movies")
+                    .setPositiveButton("ok", (dialogInterface, i) -> {
+                        ActivityCompat.requestPermissions(activity, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+                    })
+                    .setNegativeButton("cancel", (dialogInterface, i) -> {
+                        dialogInterface.dismiss();
+                    })
+                    .create().show();
+        } else {
+            ActivityCompat.requestPermissions(activity, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission granted!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission denied!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
