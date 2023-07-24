71cc6c6e2eee76a8bedfcb8bf9ae8ed05ed0e2e5
HP <wailmessaoudi806@gmail.com>
2023-07-24 12:08:01 +0100

    updated the code of the VideoPlayerActivity.java to use media3 exoplayer

--- app/src/main/java/com/example/movies/utilities/VideoPlayerActivity.java
+++ app/src/main/java/com/example/movies/utilities/VideoPlayerActivity.java
@@ -2,52 +2,47 @@ package com.example.movies.utilities;
 
 import android.net.Uri;
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.media3.common.MediaItem;
+import androidx.media3.exoplayer.ExoPlayer;
+import androidx.media3.ui.PlayerView;
 import com.example.movies.R;
-import com.google.android.exoplayer2.MediaItem;
-import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.source.ProgressiveMediaSource;
-import com.google.android.exoplayer2.ui.PlayerView;
-import com.google.android.exoplayer2.upstream.DataSource;
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
-import com.google.android.exoplayer2.util.Util;
 
 public class VideoPlayerActivity extends AppCompatActivity {
-    private SimpleExoPlayer player;
-    private PlayerView playerView;
+    private ExoPlayer player;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_player);
 
         // Get the PlayerView from the layout
-        playerView = findViewById(R.id.playerView);
+        PlayerView playerView = findViewById(R.id.playerView);
 
         // Create a SimpleExoPlayer instance
-        player = new SimpleExoPlayer.Builder(this).build();
+        player = new ExoPlayer.Builder(this).build();
 
         // Attach the player to the PlayerView
         playerView.setPlayer(player);
 
         // Get the video path from the Intent's extra data
-        String videoPath = getIntent().getStringExtra("video_path");
+        String videoPath = getIntent().getStringExtra("VIDEO_PATH");
 
         // Set the media URI to play the video (replace "video_path" with your video's local or remote path)
         Uri uri = Uri.parse(videoPath);
 
-        // Create a media source
-        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this, Util.getUserAgent(this, "Movies"));
-        MediaSource mediaSource = new ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(MediaItem.fromUri(uri));
+        // Build the media item.
+        MediaItem mediaItem = MediaItem.fromUri(uri);
 
         // Prepare the player with the media source
-        player.setMediaSource(mediaSource);
+        player.setMediaItem(mediaItem);
 
         // Prepare the player asynchronously
         player.prepare();
 
         // Start the video playback
-        player.setPlayWhenReady(true);
+        player.play();
     }
 
     @Override
