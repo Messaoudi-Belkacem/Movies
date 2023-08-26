25ffa47438c60239c7454d9a169229300095c5f6
HP <wailmessaoudi806@gmail.com>
2023-08-26 23:20:47 +0100

    transferred database and view model code to HomeFragment

--- app/src/main/java/com/example/movies/activities/MainActivity.java
+++ app/src/main/java/com/example/movies/activities/MainActivity.java
@@ -1,79 +1,36 @@
 package com.example.movies.activities;
 
-import android.app.AlertDialog;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
-import android.widget.Toast;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.room.Room;
 
-import com.example.movies.Movie;
-import com.example.movies.MovieResponse;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.movies.R;
-import com.example.movies.api.TMDBApiClient;
-import com.example.movies.api.TMDBService;
 import com.example.movies.fragments.HomeFragment;
 import com.example.movies.fragments.SearchFragment;
 import com.example.movies.fragments.WatchListFragment;
-import com.example.movies.room.AppDatabase;
-import com.example.movies.room.MovieDao;
-import com.example.movies.utilities.DialogUtilities;
-import com.example.movies.utilities.Permissions;
-import com.example.movies.viewmodels.MainViewModel;
-import com.example.movies.viewmodels.ViewModelFactory;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.HttpException;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-
-    private String tag = "MainActivity.java";
-    private MainViewModel mainViewModel;
+    private final String tag = "MainActivity.java";
     // Fragments
     private HomeFragment homeFragment;
     private SearchFragment searchFragment;
     private WatchListFragment watchListFragment;
-    // Database
-    private AppDatabase appDatabase;
-    private ArrayList<Movie> moviesDb = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        appDatabase = Room.databaseBuilder(getApplicationContext(),
-                        AppDatabase.class,
-                        "movie")
-                        .build();
-
-        mainViewModel = new ViewModelProvider(this, new ViewModelFactory(appDatabase.movieDao())).get(MainViewModel.class);
-
         homeFragment = new HomeFragment();
         searchFragment = new SearchFragment();
         watchListFragment = new WatchListFragment();
 
-        mainViewModel.getMoviesData().observe(this, (Observer<List<Movie>>) movies -> {
-            Log.d(tag, "Observed List Changed !");
-            moviesDb = (ArrayList<Movie>) movies;
-            homeFragment.setMovies(moviesDb);
-        });
-
         initializeUI();
         getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, homeFragment).commit();
 
-        Log.d("MainActivity", "onCreate method called");
+        Log.d(tag, "onCreate method called");
     }
 
     private void initializeUI() {
@@ -81,8 +38,7 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationBar.setOnItemSelectedListener(item -> {
             if (item.getItemId() == R.id.item_1) {
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, homeFragment).commit();
-                homeFragment.setMovies(moviesDb);
-                Log.d("MainActivity.java", "home fragment set !");
+                Log.d(tag, "home fragment set !");
                 return true;
             } else if (item.getItemId() == R.id.item_2) {
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, searchFragment).commit();
--- app/src/main/java/com/example/movies/fragments/HomeFragment.java
+++ app/src/main/java/com/example/movies/fragments/HomeFragment.java
@@ -10,34 +10,60 @@ import android.view.ViewGroup;
 
 import androidx.annotation.OptIn;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.media3.common.util.UnstableApi;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
+
 import com.example.movies.Movie;
 import com.example.movies.MoviesRecyclerViewAdapter;
 import com.example.movies.R;
 import com.example.movies.activities.DetailActivity;
 import com.example.movies.activities.VideoPlayerActivity;
+import com.example.movies.room.AppDatabase;
+import com.example.movies.viewmodels.MainViewModel;
+import com.example.movies.viewmodels.ViewModelFactory;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    private final String tag = "HomeFragment.java";
+    private MainViewModel mainViewModel;
     private RecyclerView moviesRecyclerView;
     private MoviesRecyclerViewAdapter moviesRecyclerViewAdapter;
     private ArrayList<Movie> movies = new ArrayList<>();
+    // Database
+    private AppDatabase appDatabase;
 
     public HomeFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        appDatabase = Room.databaseBuilder(
+                        requireContext(),
+                        AppDatabase.class,
+                        "movie"
+                        ).build();
+
+        mainViewModel = new ViewModelProvider(this, new ViewModelFactory(appDatabase.movieDao())).get(MainViewModel.class);
 
         moviesRecyclerView = rootView.findViewById(R.id.moviesRecyclerView);
         moviesRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
 
         moviesRecyclerViewAdapter = new MoviesRecyclerViewAdapter(movies, this);
         moviesRecyclerView.setAdapter(moviesRecyclerViewAdapter);
+
+        mainViewModel.getMoviesData().observe(getViewLifecycleOwner(), (Observer<List<Movie>>) movies -> {
+            Log.d(tag, "Observed List Changed !");
+            this.movies = (ArrayList<Movie>) movies;
+            moviesRecyclerViewAdapter.notifyDataSetChanged();
+        });
 
         return rootView;
     }
@@ -55,11 +81,5 @@ public class HomeFragment extends Fragment {
         Intent intent = new Intent(requireContext(), DetailActivity.class);
         intent.putExtra("MOVIE", movie);
         requireContext().startActivity(intent);
-    }
-
-    public void setMovies(ArrayList<Movie> movies) {
-        this.movies = movies;
-        moviesRecyclerViewAdapter.notifyDataSetChanged();
-        Log.d("HomeFragment.java", "notifyDataSetChanged method called");
     }
 }
